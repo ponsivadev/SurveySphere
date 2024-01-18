2d44a972a20a94100106b601b7024d8de8ca7429
subash <139846292+subash31jan@users.noreply.github.com>
2024-01-18 15:26:25 +0530

    welcomeScreen Update

---
+++ lib/pages/welcome_page/email_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:survey_sphere_app/pages/welcome_page/welcome_page.controller.dart';
+
+class EmailView extends GetView<WelcomePageController> {
+  const EmailView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      
+    );
+  }
+}
--- lib/pages/welcome_page/welcome_page.controller.dart
+++ lib/pages/welcome_page/welcome_page.controller.dart
@@ -1,15 +1,17 @@
 import 'package:get/get.dart';
 import 'package:survey_sphere_app/components/shared_preferance.dart';
 import 'package:survey_sphere_app/components/shared_preferance_key.dart';
+import 'package:survey_sphere_app/pages/welcome_page/email_view.dart';
 import 'package:survey_sphere_app/pages/welcome_page/welcome_page.variables.dart';
 
 class WelcomePageController extends GetxController with WelcomePageVariables {
   init() {}
 
-  setLanguage(String language) {
-    SharedPreferance.setString(
+  setLanguage(String language) async{
+await SharedPreferance.setString(
       keyName: SharedPreferenceKey.language,
       value: language,
     );
+      Get.to(() => const EmailView());
   }
 }
