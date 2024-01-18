d4f30e73558b96d21e0e6d5ed619261de4c9d87e
subash <139846292+subash31jan@users.noreply.github.com>
2024-01-18 17:47:03 +0530

    question

--- lib/pages/welcome_page/email_view.dart
+++ lib/pages/welcome_page/email_view.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:survey_sphere_app/components/colors.dart';
+import 'package:survey_sphere_app/components/shared_preferance.dart';
+import 'package:survey_sphere_app/components/shared_preferance_key.dart';
 import 'package:survey_sphere_app/pages/final_question_view.dart';
+import 'package:survey_sphere_app/pages/question_page/question_page.view.dart';
 import 'package:survey_sphere_app/pages/welcome_page/welcome_page.controller.dart';
 
 // Define your AppColors class (make sure it's imported where you use it)
@@ -10,7 +13,6 @@ class EmailView extends GetView<WelcomePageController> {
   EmailView({Key? key}) : super(key: key);
 
   final formKey = GlobalKey<FormState>();
-  
 
   @override
   Widget build(BuildContext context) {
@@ -27,16 +29,17 @@ class EmailView extends GetView<WelcomePageController> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                 Image.asset('assets/images/mailimage.png',
-              height: MediaQuery.of(context).size.height * 0.30,
-              width: MediaQuery.of(context).size.width * 0.30),
+                Image.asset('assets/images/mailimage.png',
+                    height: MediaQuery.of(context).size.height * 0.30,
+                    width: MediaQuery.of(context).size.width * 0.30),
                 TextFormField(
                   decoration: InputDecoration(
                     labelText: 'Email',
                     hintText: 'Enter your email',
                     // border: const OutlineInputBorder(),
                     fillColor: AppColors.cardWhite,
-                    filled: true, // Add this line to enable the fillColor for TextFormField
+                    filled:
+                        true, // Add this line to enable the fillColor for TextFormField
                     labelStyle: TextStyle(color: AppColors.primaryBlue),
                     hintStyle: TextStyle(color: AppColors.gray),
                     enabledBorder: OutlineInputBorder(
@@ -53,25 +56,33 @@ class EmailView extends GetView<WelcomePageController> {
                     if (value == null || value.isEmpty) {
                       return 'Please enter your email';
                     }
-                    if (!RegExp(r'\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b').hasMatch(value)) {
+                    if (!RegExp(r'\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b')
+                        .hasMatch(value)) {
                       return 'Please enter a valid email address';
                     }
                     return null;
                   },
+                  onChanged: (value) {
+                    // Update the controller's email value when the text changes
+                    controller.email.value = value;
+                  },
                 ),
-                SizedBox(height: 16),
+                const SizedBox(height: 16),
                 ElevatedButton(
                   style: ElevatedButton.styleFrom(
-                    primary: AppColors.primaryOrange, // Use the custom primary orange color
+                    primary: AppColors
+                        .primaryOrange, // Use the custom primary orange color
                     onPrimary: AppColors.cardWhite, // Text color
                   ),
                   onPressed: () {
                     if (formKey.currentState?.validate() ?? false) {
-                      Get.to(() => RankingPage());
-                 
+                      SharedPreferance.setString(
+                          keyName: SharedPreferenceKey.email,
+                          value: controller.email.value);
+                      Get.to(() => QuestionPage());
                     }
                   },
-                  child: Text('Proceed'),
+                  child: const Text('Proceed'),
                 ),
               ],
             ),
@@ -82,4 +93,3 @@ class EmailView extends GetView<WelcomePageController> {
     );
   }
 }
-
